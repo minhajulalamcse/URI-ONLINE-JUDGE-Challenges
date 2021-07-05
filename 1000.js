e753aa8bed858a4fed120247fb497728c77e4e09
Minhaj <minhaj@Minhajs-MacBook-Pro.local>
2021-07-06 02:38:06 +0600

    Solution added for Problem no 1000

---
+++ 1000.js
@@ -0,0 +1,15 @@
+/*
+ * URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1000
+ * Problem Name: Hello World!
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+console.log('Hello World!');
