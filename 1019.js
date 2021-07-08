f49ecd3e34c8bff58018288686b419a1bc106a18
minhajulalamcse <minhajulalam.cse@gmail.com>
2021-07-08 17:11:36 +0600

    Solution added for Problem no 1019

---
+++ 1019.js
@@ -0,0 +1,34 @@
+/*
+ * URL: 
+ * Problem Name: 
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var seconds = parseInt(lines.shift());
+
+function getRemainder(a, b){
+    return a%b;
+}
+function getDivision(a, b){
+    return parseInt(a/b);
+}
+
+var remainder = 0;
+
+var hours = getDivision(seconds, 3600);
+remainder = getRemainder(seconds, 3600);
+
+var minutes = getDivision(remainder, 60);
+remainder = getRemainder(remainder, 60);
+
+var seconds = remainder;
+
+console.log(hours+':'+minutes+':'+seconds);
