3eb6f6c1d86f8db3b63496de827ccc3fdd295113
minhajulalamcse <minhajulalam.cse@gmail.com>
2021-07-09 18:44:12 +0600

    Solution added for Problem no 1059

---
+++ 1059.js
@@ -0,0 +1,17 @@
+/*
+ * URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1059
+ * Problem Name: 1059 - Even Numbers
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+for(var i = 2; i<=100; i = i + 2){
+    console.log(i);
+}
