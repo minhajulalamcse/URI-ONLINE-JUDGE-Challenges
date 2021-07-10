404327ae5dfd7bd2e5f19de759b4906f654ba7ec
minhajulalamcse <minhajulalam.cse@gmail.com>
2021-07-11 00:51:47 +0600

    Solution added for Problem no 1052

---
+++ 1052.js
@@ -0,0 +1,19 @@
+/*
+ * URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1052
+ * Problem Name: 1052 - Month
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var N = parseInt(lines.shift());
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+console.log(months[N-1]);
