290e7074f73f225ccd50757793beb7051299368d
minhajulalamcse <minhajulalam.cse@gmail.com>
2021-07-15 02:11:42 +0600

    Solution added for Problem no 1924

---
+++ 1924.js
@@ -0,0 +1,20 @@
+/*
+ * URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1924
+ * Problem Name: 1924 - Vitória and Her Indecision
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var T = parseInt(lines.shift());
+
+for(var i = 0; i<T; i++){
+    lines.shift();
+}
+console.log('Ciencia da Computacao');
