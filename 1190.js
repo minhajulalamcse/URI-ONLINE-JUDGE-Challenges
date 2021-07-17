a6da8d6047826431ee87e8b2922ca98a6fd93e0c
minhajulalamcse <minhajulalam.cse@gmail.com>
2021-07-17 22:47:52 +0600

    Solution added for Problem no 1190

---
+++ 1190.js
@@ -0,0 +1,51 @@
+/*
+ * URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1190
+ * Problem Name: 1190 - Right Area
+ */
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+
+ var operation = lines.shift();
+
+ var positions = [];
+ var value = 0;
+ var m = 1;
+ for(var i = 1; i<11; i++){
+    value = i * 12;
+    if(i<=5){
+      for(var j = 11; j>11-i; j--){
+        positions.push(value + j);
+      }
+    }else{
+      for(var j = 11; j>11-i+m; j--){
+        positions.push(value + j);
+      }
+      m += 2;
+    }
+ }
+
+
+ var sum = 0;
+ var count = 0;
+ 
+ for(var i = 0; i<144; i++){
+     var num = parseFloat(lines.shift());
+     if(positions.indexOf(i)!=-1){
+         sum += num;
+         count++;
+     }
+ }
+ 
+ if(operation=='S'){
+     console.log(sum.toFixed(1));
+ }else{
+     console.log((sum/count).toFixed(1));
+ }
