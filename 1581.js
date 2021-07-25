98fc819dc37428a776e15167c739a13bab493fdd
minhajulalamcse <minhajulalam.cse@gmail.com>
2021-07-26 02:24:46 +0600

    Solution added for Problem no 1581

---
+++ 1581.js
@@ -0,0 +1,36 @@
+/*
+ * URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1581
+ * Problem Name: 1581 - International Chat
+ */
+
+var input = require('fs').readFileSync('./dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var N = parseInt(lines.shift());
+
+for(var i = 0; i<N; i++){
+    var k = parseInt(lines.shift());
+    var isSame = true;
+    var firstLanguage = '';
+    for(var j = 0; j<k; j++){
+        var language = lines.shift();
+        if(j===0){
+            firstLanguage = language;
+        }else{
+            if(language !== firstLanguage){
+                isSame = false;
+            }
+        }
+    }
+    if(isSame){
+        console.log(firstLanguage);
+    }else{
+        console.log('ingles');
+    }
+}
